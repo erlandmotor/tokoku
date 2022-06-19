5eca92ea9167d372a46b902e02b2abb80264fbcb
anggitpp <anggitpp23@gmail.com>
2022-06-19 11:19:40 +0700

    add firebase plugin to app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewExample extends StatefulWidget {
@@ -22,7 +23,7 @@ class WebViewExample extends StatefulWidget {
 class _WebViewExampleState extends State<WebViewExample> {
   bool isLoading = true;
 
-  final Completer<WebViewController> _controller =
+  final Completer<WebViewController> _completerController =
       Completer<WebViewController>();
 
   @override
@@ -33,35 +34,70 @@ class _WebViewExampleState extends State<WebViewExample> {
     }
   }
 
+  WebViewController? _controller;
+
+  Future<bool> _handleBack(context) async {
+    var status = await _controller!.canGoBack();
+    if (status) {
+      _controller!.goBack();
+      return Future.value(false);
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Do you want to exit'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        ),
+      );
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            WebView(
-              initialUrl: 'https://ppob.teratesejahtera.com/',
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (WebViewController webViewController) {
-                _controller.complete(webViewController);
-              },
-              gestureNavigationEnabled: true,
-              backgroundColor: const Color(0x00000000),
-              onPageFinished: (finish) {
-                setState(() {
-                  isLoading = false;
-                });
-              },
-              onProgress: (progress) {
-                print('$progress %');
-              },
-            ),
-            isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Stack(),
-          ],
+    return WillPopScope(
+      onWillPop: () => _handleBack(context),
+      child: Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              WebView(
+                initialUrl: 'https://ppob.teratesejahtera.com/',
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _completerController.complete(webViewController);
+                },
+                gestureNavigationEnabled: true,
+                backgroundColor: const Color(0x00000000),
+                onPageFinished: (finish) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+                onProgress: (progress) {
+                  debugPrint('$progress %');
+                },
+              ),
+              isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Stack(),
+            ],
+          ),
         ),
       ),
     );
